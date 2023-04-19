691de5a562749f678861e5d82a87f77ccf9ee3dd
Delia Paternina <delia@Juans-MacBook-Air.local>
2023-04-19 14:46:57 +0100

    defines your_module_ajax_callback method

--- web/modules/dictionary/src/Form/DictionaryForm.php
+++ web/modules/dictionary/src/Form/DictionaryForm.php
@@ -4,6 +4,8 @@ namespace Drupal\dictionary\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use GuzzleHttp\Client;
 
 /**
  * Provides a form for calling an API with an input and updating a block.
@@ -30,12 +32,30 @@ class DictionaryForm extends FormBase {
       '#type' => 'submit',
       '#value' => t('Submit'),
       '#ajax' => [
-        'callback' => 'my_api_module_update_response_block',
+        'callback' => [$this, 'myCallbackMethod'],
         'wrapper' => 'my-api-response',
       ],
     ];
     return $form;
   }
+
+  public function myCallbackMethod(array &$form, FormStateInterface $form_state) {
+    $input = $form_state->getValue('input');
+
+    // Make an API call with GuzzleHttp\Client.
+    $client = new Client(['base_uri' => 'https://api.dictionaryapi.dev/api/v2/entries/en/']);
+    $response = $client->request('GET', $input);
+
+    // Get the response body.
+    $response_body = (string) $response->getBody();
+
+    // Return the response as an Ajax command.
+    $ajax_response = new \Drupal\Core\Ajax\AjaxResponse();
+    $ajax_response->addCommand(new \Drupal\Core\Ajax\HtmlCommand('#my-api-response', $response_body));
+
+    return $ajax_response;
+  }
+
 
   /**
    * {@inheritdoc}
