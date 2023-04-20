e61da658dd1cf1b8e73157c6042a89b6f80ab6ed
Delia Paternina <delia@Juans-MacBook-Air.local>
2023-04-20 20:20:01 +0100

    cleans up

--- web/modules/dictionary/src/Plugin/Block/DictionaryBlock.php
+++ web/modules/dictionary/src/Plugin/Block/DictionaryBlock.php
@@ -35,12 +35,3 @@ class DictionaryBlock extends BlockBase {
   }
 
 }
-
-// This file defines a Drupal block plugin that provides a block for calling an API with an input and updating the response. It extends the BlockBase
-//  class and defines a build method which returns a render array for the block.
-
-// In the build method, a form object is obtained by calling getForm on the formBuilder service, and a container element is created for the response
-// with an ID of "my-api-response". Both the form and response are returned as elements in an associative array, with the keys "form" and "response",
-// respectively.
-
-// The block is identified by its ID, which is "my_api_block", and its admin label is set to "My API Block".
